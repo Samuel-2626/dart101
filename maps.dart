52199eaf798738b939f1d78973c3efcace6a05e2
Samuel Torimiro <efosonaname@gmail.com>
2024-01-24 23:11:44 -0600

    maps

---
+++ maps.dart
@@ -0,0 +1,21 @@
+void main() {
+
+  Map<String, String> planets = {
+    "first": "mercury",
+    "second": "venus",
+    "third": "earth",
+    "fourth": "mars",
+    "fifth": "jupiter",
+    "sixth": "uranus"
+  };
+
+  print(planets);
+
+  print(planets["second"])
+
+  print(planets.containsKey("first"));
+  print(planets.containsValue("earth"));
+  print(planets.keys);
+  print(planets.values);
+
+}
