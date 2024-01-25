3e0505dcd5cb57949c93dcb1a6b1e641c846b50c
Samuel Torimiro <efosonaname@gmail.com>
2024-01-24 19:41:21 -0600

    dart basics

---
+++ dart-basic.dart
@@ -0,0 +1,13 @@
+void main() {
+
+    var name = "Mario";
+
+    print(name);
+
+    final age = 25; // run-time constant
+
+    const isOpen = true; // compiled time constant
+
+    print("My name is $name");  // string interpolation
+
+}
